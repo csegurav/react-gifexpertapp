559cb735693ed20c6599720cde12fd2801c8d4b9
carlos <cibercarlossv@gmail.com>
2022-03-25 11:33:02 -0400

    First Commit

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+const GiftExpertApp = () => {
+  const [categories, setCategories] = useState(["One Ponch"]);
+
+  return (
+    <>
+      <h1>GiftExpertApp</h1>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default GiftExpertApp;
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=H9Ly8D583tH7U1jKZHADXqdDRZydEK3X`;
+
+  const respuesta = await fetch(url);
+  const { data } = await respuesta.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
